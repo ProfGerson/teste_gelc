1c0766cc2af4aee0170dc9b30a21a8cfe68d0ea1
ProfGerson <prof.gersonfernandes@gmail.com>
2024-06-14 16:14:27 -0300

    Update Script_Gelc.js

--- Script_Gelc.js
+++ Script_Gelc.js
@@ -1,52 +1,16 @@
-PennController.ResetPrefix(null);
+PennController.ResetPrefix(null)
+PennController.DebugOff();
 
 Sequence("Participante", "Instrucoes", randomize("Experimento"), SendResults(), "Final");
 
-Header(
-         defaultText
-                  .css("font-size","1.2em")
-                  .print()
-         ,
-         defaultTextInput
-                  .css("font-size","1.2em")
-                  .print()
-         ,
-         defaultButton
-                  .css("font-size","1.2em")
-                  .center()
-                  .print()
-                  .wait()   
-)
-
-NewTrial("Participante")
-
-    newtext("<p>Olá! Seja bem-vindo ao nosso experimento!</p>")
-    ,
-    newtext("<p>Por gentileza, informe o seu NOME COMPLETO.</p>")
-    ,
-    newTextInput("Nome")
-    ,
-    newtext("<p>O que você cursa na Ufersa?</p>")
-    ,
-    newTextInput("Curso")
-    ,
-    newButtonn ("Iniciar")
-    ,
-    newVar ("NOME")
-      .global() 
-      .set( gettextinput("Nome"))
-)
-
-.log("NOME" , getVar("NOME"))
-
-newtrial ("Instrucoes",
-
-          newtext ("<p> Instruções </p>") 
-          ,
-          newtext ("<p>Para a realização, você será apresentado a uma situação fictícia e em seguida lerá em voz alta a sentença que aparece em destaque.</p>") 
-          ,
-
-          newButton ("Iniciar")
-            .log()
-)
-
+NewTrial("Participante") ,
+    DefaulText
+        .css("font-size","1.2em") 
+        .print
+Newtrial ("Instrucoes")
+     DefaulText
+        .css("font-size","1.2em") 
+        .print
+    newText("<p>>Para a realização, você será apresentado a uma situação fictícia e em seguida lerá em voz alta a sentença que aparece em destaque.</p>")
+    
+    
